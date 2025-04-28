7e8e8e5af61f18cee4e7a6ba0d73e5584ecad19a
l-you <32598874+l-you@users.noreply.github.com>
2025-04-28 12:49:20 +0300

    Update generator.js

--- generator.js
+++ generator.js
@@ -2,7 +2,8 @@ let txt = '';
 for (const item of []) {
   txt += `### ${item.title}\n\n`;
   for (const {slug,title,link} of item.stack) {
-    txt += `<a href="${link}" target="_blank" rel="noopener noreferrer">
+    txt += `
+<a href="${link}" target="_blank" rel="noopener noreferrer">
   <picture>
     <source srcset="https://cdn.simpleicons.org/${slug}/dark" media="(prefers-color-scheme: light)">
     <source srcset="https://cdn.simpleicons.org/${slug}/white" media="(prefers-color-scheme: dark)">
